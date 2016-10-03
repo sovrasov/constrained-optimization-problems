114f700759f034a4b9ab11f75b10f93575f46091
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2016-10-03 14:22:56 +0300

    Cleanup in ConstrainedProblemGenerator

--- src/problem_generator.hpp
+++ src/problem_generator.hpp
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <cmath>
+#include <algorithm>
+
 #include "constrained_problem.hpp"
 
 enum GenerateMode { RHS = 0, DELTA = 1 };
@@ -17,16 +19,17 @@ protected:
   std::vector<double> mConstraintsParams;
   std::vector<bool>   mNeedTuneParam;
 
-  double EvaluateRHS(FType* function, double delta, double objectiveMin)
+  double CalculateRHS(FType* function, double delta, double objectiveMin)
   {
-    double q;
-    double Epsilon = 0.01; int m = 100;
+    double rhs;
+    double Epsilon = 0.01;
+    int m = 100;
     int dimension = function->GetDimension();
     double hmin = function->GetOptimalValue();
 
     double hmax = hmin;
     double d = 0;
-    //многомерная решетка, в узлах - испытания,
+    //многомерная решетка, в узлах - испытания
     int* size = new int[dimension];//кол-во колво точек на размерность
     double* step = new double[dimension];//шаг по каждой размерности
     int sumn = 1;//число испытаний
@@ -37,13 +40,13 @@ protected:
     for (unsigned i = 0; i < dimension; i++)
     {
       d = (b[i] - a[i]);
-      size[i] = (int)ceil(d / Epsilon) + 1;//(int)ceil(d / Epsilon) + 1;
+      size[i] = (int)ceil(d / Epsilon) + 1;
       step[i] = d / (size[i] - 1);
       sumn *= (size[i]);
     }
     double* f = new double[sumn];//значение функции
     double* y = new double[dimension];
-    //#pragma omp parallel for num_threads(parameters.NumThread)
+
     for (int i = 0; i < sumn; i++)
     {
       double w;
@@ -51,16 +54,14 @@ protected:
       //Вычисляе координаты точек испытания
       for (unsigned j = 0; j < dimension; j++)
       {
-        w = z % size[j];//определяем номер узла на i-ой размерносте
-        y[j] = a[j] + w * step[j];//левая граница + номер узла на i-ой размерносте * шаг на i-ой размерносте
-        z = z / size[j];//для вычисления номера узла на следующей оазмерносте
+        w = z % size[j];//определяем номер узла на i-ой размерности
+        y[j] = a[j] + w * step[j];//левая граница + номер узла на i-ой размерности * шаг на i-ой размерности
+        z = z / size[j];//для вычисления номера узла на следующей размерности
       }
       //проводим испытание
       f[i] = function->Calculate(y);
-      if (f[i] > hmax)
-        hmax = f[i];
-      if (f[i] < hmin)
-        hmin = f[i];
+      hmax = std::max(f[i], hmax);
+      hmin = std::min(f[i], hmin);
     }
 
     double* h1 = new double[m];
@@ -79,16 +80,12 @@ protected:
     }
 
     for (int i = 0; i < sumn; i++)
-    {
       for (int j = 0; j < m; j++)
-      {
         if ((f[i] >= h1[j]) && (f[i] <= h2[j]))
         {
           p[j] ++;
           break;
         }
-      }
-    }
 
     s[0] = p[0];
     for (int i = 1; i < m; i++)
@@ -98,27 +95,22 @@ protected:
 
     double smax = s[m - 1];
     double g = delta * smax;
-    double globalMin = objectiveMin;
     for (int i = 0; i < m; i++)
     {
       if (s[i] >= g)
       {
-        q = h2[i];
+        rhs = h2[i];
         break;
       }
     }
 
-    //printf("In constraint %d q = %lf\n", constraintNum, q);
     double dm = delta;
     if (dm == 0)
       dm += 0.1;
     dm = dm * (hmax - hmin);
 
-    if (q < globalMin)
-    {
-      q = globalMin + dm;
-      //printf("In constraint %d replace q  = %lf\n", constraintNum, q);
-    }
+    if (rhs < objectiveMin)
+      rhs = objectiveMin + dm;
 
     delete[] size;
     delete[] step;
@@ -132,7 +124,7 @@ protected:
     delete[] p;
     delete[] s;
 
-    return q;
+    return rhs;
   }
 
 public:
@@ -158,7 +150,7 @@ public:
     for(unsigned i = 0; i < mPConstraints.size(); i++)
     {
       if(mNeedTuneParam[i])
-        mConstraintsParams[i] = EvaluateRHS(mPConstraints[i], mConstraintsParams[i], objectiveMinVal);
+        mConstraintsParams[i] = CalculateRHS(mPConstraints[i], mConstraintsParams[i], objectiveMinVal);
     }
 
     return ConstrainedProblem<FType>(mPObjective, mPConstraints, mConstraintsParams);
